8738770bf1dd9770b7650a84cc7eb6d2a655e052
Brooks Sime <brookssime@gmail.com>
2015-11-02 15:43:44 -0500

    Trigger question yes and no buttons are created programmatically, done button moves to patient management

--- app/src/main/java/cs408/alertaide/Trig_Ques_Activity.java
+++ app/src/main/java/cs408/alertaide/Trig_Ques_Activity.java
@@ -2,13 +2,16 @@ package cs408.alertaide;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import cs408.alertaide.backend.AAException;
 import cs408.alertaide.backend.AA_Manager;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -17,6 +20,7 @@ import java.util.Iterator;
 
 public class Trig_Ques_Activity extends Activity {
     LinearLayout linear;
+    //int [] check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,55 +30,129 @@ public class Trig_Ques_Activity extends Activity {
         try {
             AA_Manager myManager = new AA_Manager(this);
             JSONObject tq = myManager.getTQs("pphem");
-            askQuestion(tq);
+            ask_Question(tq);
         } catch (AAException e) {
-           throwError(e.getMessage());
+            throw_Error(e.getMessage());
         }
 
     }
 
-    private void askQuestion(JSONObject json) {
+
+    private void throw_Error(String errorMessage) {
+
+        AA_ErrorPopup errorPopup = new AA_ErrorPopup(this, errorMessage);
+    }
+
+    private void set_Question_Layout(String question) {
+        TextView myText = new TextView(this);
+        myText.setText(question);
+        linear.addView(myText);
+    }
+
+    private void set_Answer_Layout(JSONArray answer) throws JSONException {
+        LinearLayout buttonLayout = new LinearLayout(this);
+        buttonLayout.setOrientation(LinearLayout.HORIZONTAL);
+
+        for (int i = 0; i < answer.length(); i++) {
+            Button myButton = new Button(this);
+            String buttonText = answer.getString(i);
+            myButton.setText(buttonText);
+            buttonLayout.addView(myButton);
+        }
+
+        linear.addView(buttonLayout);
+    }
+
+    private void finish_TQ() {
+        Button done = new Button(this);
+        done.setText("Done");
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goto_pmanagement();
+            }
+        });
+        linear.addView(done);
+    }
+
+        /**
+         * Goes through maximum of two nested linear layouts, only checks Yes No answers.
+         * If click Done Button goes to patient management.
+         * @param layout
+         */
+    public void is_Clicked(LinearLayout layout) {
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            View v = layout.getChildAt(i);
+            if (v instanceof LinearLayout) {
+                for (int j = 0; j < ((LinearLayout) v).getChildCount(); j++) {
+
+                    View k = ((LinearLayout) v).getChildAt(j);
+                    if (k instanceof Button) {
+                        if (((Button) k).getText() == "Yes") {
+
+                            k.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View k) {
+                                       k.setBackgroundColor(Color.parseColor("#00FF00"));
+                                    //TODO: Log choice
+                                }
+                            });
+                        }
+
+                        if (((Button) k).getText() == "No")
+
+                        {
+                            k.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View k) {
+                                    k.setBackgroundColor(Color.parseColor("#ff0000"));
+                                    //TODO: Log choice
+                                }
+                            });
+                        }
+
+
+                    }
+                }
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+
+
+
+    private void ask_Question(JSONObject json) {
         Iterator<String> iter = json.keys();
         while (iter.hasNext()) {
             String key = iter.next();
             try {
                 JSONObject value = (JSONObject) json.get(key);
                 String question = value.getString("question");
-                askYesNo(question);
+                JSONArray answer = value.getJSONArray("answer_options");
+                set_Question_Layout(question);
+                set_Answer_Layout(answer);
+
+
+
 
 
             } catch (JSONException e) {
-                throwError(e.getMessage());
+                throw_Error(e.getMessage());
 
             }
         }
-    }
-
-    private void throwError(String errorMessage) {
-        AA_ErrorPopup errorPopup = new AA_ErrorPopup(this, errorMessage);
-    }
-
-    private void askYesNo(String question) {
-        //TODO: add yes no through app_data xml (JSONARRAY)
-        TextView myText = new TextView(this);
-        myText.setText(question);
-        linear.addView(myText);
-        LinearLayout buttonLayout = new LinearLayout(this);
-        buttonLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-        Button yes = new Button(this);
-        Button no = new Button (this);
-
-        yes.setText("Yes");
-        no.setText("No");
-
-
-        buttonLayout.addView(yes);
-        buttonLayout.addView(no);
-
-        linear.addView(buttonLayout);
-
-
+         is_Clicked(linear);
+         finish_TQ();
     }
 
     @Override
