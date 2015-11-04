6dd5a58f023caaaec996a41aa1e969e8057b600d
Yossra Hamid <yossraih@gmail.com>
2015-11-04 17:41:11 -0500

    PM Stuff

--- app/src/main/java/cs408/alertaide/PManagement_Activity.java
+++ app/src/main/java/cs408/alertaide/PManagement_Activity.java
@@ -7,23 +7,73 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import cs408.alertaide.backend.AAException;
+import cs408.alertaide.backend.AA_Manager;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.Iterator;
 
 public class PManagement_Activity extends Activity {
+    LinearLayout linear;
+    JSONObject pmAnswers;
+    AA_Manager myManager;
+    Bundle myBundle;
     Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pmanagement);
-        done = (Button) findViewById(R.id.done);
+        linear = (LinearLayout) findViewByID(R.id.linear);
+        myBundle = getIntent().getExtras();
+
+        /*done = (Button) findViewById(R.id.done);
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goto_checkout();
             }
-        });
+        });*/
+
+        try {
+            myManager = new AA_Manager(this);
+            String condition = myBundle.getString("condition");
+            JSONObject pm = myManager.getPMs(condition);
+            setPM(pm);
+
+        }catch (AAException e) {
+            throw_Error(e.getMessage());
+        }
     }
+
+    private void throw_Error(String errorMessage) {
+
+        AA_ErrorPopup errorPopup = new AA_ErrorPopup(this, errorMessage);
+    }
+
+
+
+    private void setPM(JSONObject json) {
+        Iterator<String> iter = json.keys();
+        while (iter.hasNext()) {
+            String key = iter.next();
+            try {
+                JSONObject value = (JSONObject) json.get(key);
+                String action =
+
+            } catch (JSONException e) {
+                throw_Error(e.getMessage());
+            }
+        }
+    }
+
+
+
 
     private void goto_checkout(){
         Intent intent = new Intent(this, Checkout_Activity.class);
