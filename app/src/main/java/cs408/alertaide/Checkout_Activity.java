6359344402d039106171abe05e3bbf53d9026cac
Yossra Hamid <yossraih@gmail.com>
2015-11-16 05:45:42 -0500

    Checkout - mostly functional (button colors still need work)

--- app/src/main/java/cs408/alertaide/Checkout_Activity.java
+++ app/src/main/java/cs408/alertaide/Checkout_Activity.java
@@ -2,6 +2,8 @@ package cs408.alertaide;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +13,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Iterator;
@@ -21,68 +24,50 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
-public class Checkout_Activity extends Activity {
+public class Checkout_Activity extends Activity implements OnClickListener {
 
     LinearLayout myLayout;
     AA_Manager myManager;
-    String myCondition;
-    String myFile;
-    JSONObject myPMJson;
-
-    int nextCE;
+    JSONObject myCheckout;
     LinearLayout.LayoutParams layoutParams;
+    String checkoutanswer;
+    Bundle myBundle;
 
-    TitleView myTitle;
+    AAView myTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
 
+        myCheckout = new JSONObject();
+
+        checkoutanswer = "default";
+
+        ImageButton btn = (ImageButton)findViewById(R.id.contact_treatment);
+        btn.setOnClickListener(this); // calling onClick() method
+        ImageButton btn1 = (ImageButton)findViewById(R.id.nocontact_treatment);
+        btn1.setOnClickListener(this); // calling onClick() method
+        ImageButton btn2 = (ImageButton)findViewById(R.id.contact_notreatment);
+        btn2.setOnClickListener(this); // calling onClick() method
+        ImageButton btn3 = (ImageButton)findViewById(R.id.nocontact_notreatment);
+        btn3.setOnClickListener(this); // calling onClick() method
 
 
-        myLayout = (LinearLayout) findViewById(R.id.myLayout);
-        myLayout.setPadding(0, 100, 0, 100);
-        layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(25, 75, 25, 75);
-
-        myTitle = new TitleView(this, "CHECK OUT");
-        myLayout.addView(myTitle, layoutParams);
-
-        /*
-        createCheckout();
-
-        try {
-
-            myManager = new AA_Manager(this);
-            myPMJson = myManager.getPMs(myCondition);
-
-        } catch (Exception e){
-            throw_Error("Failed to start Patient Management \n" + e.getMessage());
-        }*/
     }
+
+
+
 
     private void throw_Error(String errorMessage) {
         AA_ErrorPopup errorPopup = new AA_ErrorPopup(this, errorMessage);
     }
 
-    private void createCheckout(){
-        /*
-        try {
-            Iterator<String> iterator = myPMJson.keys();
-            while (iterator.hasNext()) {
-                String key = iterator.next();
-                JSONObject pm = myPMJson.getJSONObject(key);
-                PMView pmView = new PMView(this, pm);
-                myLayout.addView(pmView, layoutParams);
-                //add view to layout and arraylist for log grabbing;
-            }
-        } catch (Exception e){
-            throw_Error("Error while creating PM views \n" + e.getMessage());
-        }*/
-    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -104,28 +89,80 @@ public class Checkout_Activity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-    /** Called when the user clicks any button on Check out page, ends the session in app */
-    public void exit(View view) {
-        switch(view.getId())
-        {/*
-            case R.id.contact_treatment:
 
+    private void timeStamp () throws JSONException {
+        Long end = System.currentTimeMillis();
+        String time = end.toString();
+        myCheckout.put("timeStamp", time);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        // default method for handling onClick Events..
+
+
+        switch(v.getId())
+        {
+            case R.id.contact_treatment:
+                if(v.getBackground().equals(Color.GREEN))
+                {
+                    v.setBackgroundColor(Color.TRANSPARENT);
+                }
+
+                else {
+                    v.setBackgroundColor(Color.GREEN);
+                }
+                checkoutanswer = "Successful CE Contact, Successful Patient Treatment";
 
                 break;
 
             case R.id.contact_notreatment:
                 // Code for button 3 click
+                v.setBackgroundColor(Color.GREEN);
+                checkoutanswer = "Successful CE Contact, Unsuccessful Patient Treatment";
+
                 break;
 
             case R.id.nocontact_treatment:
                 // Code for button 3 click
+                v.setBackgroundColor(Color.GREEN);
+                checkoutanswer = "Unsuccessful CE Contact, Successful Patient Treatment";
                 break;
 
             case R.id.nocontact_notreatment:
                 // Code for button 3 click
-                break; */
+                v.setBackgroundColor(Color.GREEN);
+                checkoutanswer = "Unsuccessful CE Contact, Unsuccessful Patient Treatment";
+                break;
+        }
+    }
+
+    /** Called when the user clicks any button on Check out page, ends the session in app */
+    public void exit(View view) throws JSONException {
+        //AAButton done = new AAButton(this, "Done");
+
+        //Logging
+        try {
+            timeStamp();
+            myCheckout.put("selection", checkoutanswer);
+
+            /* Checking Logging
+            Toast.makeText(Checkout_Activity.this,
+                    myCheckout.toString(), Toast.LENGTH_LONG).show(); */
+
+            myManager.logInfo(myBundle.getString("file"), "checkOut", myCheckout);
+        }
+        catch (Exception e){
+            throw_Error(e.getMessage());
         }
 
+
+        //Sends it back to the conditions page
+        Intent intent = new Intent(this, Condition_Activity.class);
+        Bundle extras = new Bundle();
+        startActivity(intent);
 
     }
 }
